d2149d800c721b4013aef4fd0629f17e34b60907
YagoMP88 <yagomartins013@gmail.com>
2026-04-07 19:01:03 -0300

    Seller entity class

--- src/application/program.java
+++ src/application/program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
 	
-		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		Department obj = new Department(1,"books");
+		Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
