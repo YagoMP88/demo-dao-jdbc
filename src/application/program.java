8dc56190b08b1208951e161b9fcc67fef0b2c618
YagoMP88 <yagomartins013@gmail.com>
2026-04-08 19:22:59 -0300

    insert implementation

--- src/application/program.java
+++ src/application/program.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -33,6 +34,11 @@ public class program {
 			System.out.println(obj);
 		}
 		
+		
+		System.out.println("\n========== TEST 3: SELLER insert ============");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerdao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
